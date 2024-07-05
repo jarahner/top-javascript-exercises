ada593eea89bd21b563941b89d905bdb5be95867
Jannik Rahner <18661139+jarahner@users.noreply.github.com>
2024-07-05 16:34:54 +0200

    Results from Fundamentals Part 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 400 && !(year % 100)) {
+    return false;
+  } else if (year % 4) return false;
+  else return true;
 };
 
 // Do not edit below this line
